31c59e0666f8aec8d91917fda018bcda710e4376
chenzhou <chenzhou@thoughtworks.com>
2017-05-21 16:25:01 +0800

    [chen] finish disposable.

--- src/BanKai.Basic/18_Disposable.cs
+++ src/BanKai.Basic/18_Disposable.cs
@@ -25,7 +25,8 @@ namespace BanKai.Basic
             }
 
             // change variable value to fix test.
-            const string expectedTracingMessage = "";
+            const string expectedTracingMessage = "constructor called.\r\n" +
+                                                  "dispose called.\r\n";
 
             Assert.Equal(expectedTracingMessage, tracer.ToString());
         }
@@ -41,7 +42,8 @@ namespace BanKai.Basic
             }
 
             // change the variable value to fix the test.
-            const string expectedTracingMessage = "";
+            const string expectedTracingMessage = "constructor called.\r\n" +
+                                                  "dispose called.\r\n";
 
             Assert.Equal(expectedTracingMessage, tracer.ToString());
         }
