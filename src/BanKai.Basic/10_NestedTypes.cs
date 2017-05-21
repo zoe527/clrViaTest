db1944ed9a97c95092764d1824e01ff8d5931962
chenzhou <chenzhou@thoughtworks.com>
2017-05-21 15:48:21 +0800

    [chen] finish nested types.

--- src/BanKai.Basic/10_NestedTypes.cs
+++ src/BanKai.Basic/10_NestedTypes.cs
@@ -14,7 +14,7 @@ namespace BanKai.Basic
             string fullName = typeOfNestedType.FullName;
 
             // correct the value of expectedFullName to fix the test.
-            const string expectedFullName = "BanKai.Basic.Common.NestedTypeDemoClass.NestedType";
+            const string expectedFullName = "BanKai.Basic.Common.NestedTypeDemoClass+NestedType";
 
             Assert.Equal(expectedFullName, fullName);
         }
