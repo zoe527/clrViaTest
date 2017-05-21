ba748dee348a6e5dd782a7750fea6d81ba81c3d7
chenzhou <chenzhou@thoughtworks.com>
2017-05-21 16:12:40 +0800

    [chen] finish delegates related.

--- src/BanKai.Basic/13_DelegatesRelated.cs
+++ src/BanKai.Basic/13_DelegatesRelated.cs
@@ -30,7 +30,7 @@ namespace BanKai.Basic
             TransformerDelegateDemo transformer = EquivalentTransformation;
 
             // change variable value to fix test.
-            const string expectedDelegateType = "";
+            const string expectedDelegateType = "TransformerDelegateDemo";
 
             Assert.Equal(expectedDelegateType, typeof(TransformerDelegateDemo).Name);
             Assert.True(transformer is Delegate);
@@ -44,7 +44,7 @@ namespace BanKai.Basic
             int transformResult = transformer(2);
 
             // change variable value to fix test.
-            const int expectedResult = default(int);
+            const int expectedResult = 2;
 
             Assert.Equal(expectedResult, transformResult);
         }
@@ -57,7 +57,7 @@ namespace BanKai.Basic
             int actualResult = PassingDelegateAsArgument(transformer);
 
             // change variable value to fix test.
-            const int expectedResult = default(int);
+            const int expectedResult = 5;
 
             Assert.Equal(expectedResult, actualResult);
         }
@@ -73,7 +73,7 @@ namespace BanKai.Basic
             theDelegate();
 
             // change variable value to fix test.
-            var expectedTrace = new string[] {};
+            var expectedTrace = new string[] { "MulticastDelegateDemoClass.OneMethod() called", "MulticastDelegateDemoClass.AnotherMethod() called"};
 
             Assert.Equal(expectedTrace, demoObject.Trace);
         }
@@ -90,7 +90,7 @@ namespace BanKai.Basic
             theDelegate();
 
             // change variable value to fix test.
-            var expectedTrace = new string[] { };
+            var expectedTrace = new string[] { "MulticastDelegateDemoClass.AnotherMethod() called" };
 
             Assert.Equal(expectedTrace, demoObject.Trace);
         }
@@ -106,12 +106,12 @@ namespace BanKai.Basic
             theDelegate += demoObject.AnotherMethod;
 
             // change variable value to fix test.
-            const bool areReferenceEqual = true;
+            const bool areReferenceEqual = false;
 
             Assert.Equal(areReferenceEqual, ReferenceEquals(theDelegate, copy));
         }
 
-        [Fact]
+        [Fact]/*need rethink*/
         public void should_be_list_subtraction_rather_than_scalar_substraction()
         {
             var demoObject = new DelegateSubtractionDemoClass();
@@ -123,12 +123,12 @@ namespace BanKai.Basic
             ((a + b + c) - (a + c))();
 
             // change variable value to fix test.
-            const string expectedOutput = "B";
+            const string expectedOutput = "A,B,C";
 
             Assert.Equal(expectedOutput, demoObject.ToString());
         }
 
-        [Fact]
+        [Fact]/*need rethink*/
         public void should_get_result_of_last_called_function()
         {
             var demoObject = new MulticastFuncDelegateDemoClass();
@@ -140,7 +140,7 @@ namespace BanKai.Basic
             int returnedResult = (returnsOne + returnsThree + returnsTwo)();
 
             // change variable value to fix test.
-            const int expectedResult = 6;
+            const int expectedResult = 2;
 
             Assert.Equal(expectedResult, returnedResult);
         }
@@ -155,7 +155,7 @@ namespace BanKai.Basic
             object returnedValue = delegateReturnsObject();
 
             // change variable value to fix test.
-            object expectedValue = null;
+            object expectedValue = "Hello";
 
             Assert.Equal(expectedValue, returnedValue);
         }
@@ -171,7 +171,7 @@ namespace BanKai.Basic
             string returnedValue = delegateAcceptsString("Good");
 
             // change variable value to fix test.
-            const string expectedValue = "";
+            const string expectedValue = "Good";
 
             Assert.Equal(expectedValue, returnedValue);
         }
